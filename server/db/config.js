880d99ca32c2a902b6a546cb04016bf6b437301b
Martin Fracker <martin.frackerjr@gmail.com>
2018-03-26 04:21:55 -0500

    Grab pg host from DATABASE_HOST env var
    [ci-skip]

--- server/db/config.js
+++ server/db/config.js
@@ -27,7 +27,7 @@ export default {
   production: {
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
+      host: process.env.DATABASE_HOST,
       user: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME
